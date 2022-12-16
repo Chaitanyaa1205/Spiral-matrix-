7cda5c935149ff1c37ff34141cffd3c328ac7a6b
Chaitanya Gore <114816461+Chaitanyaa1205@users.noreply.github.com>
2022-12-16 16:03:01 +0530

    Add files via upload
    Staircase search is the efficient searching technique in the 2D array with the less time complexity

---
+++ StaircaseSearch.java
@@ -0,0 +1,38 @@
+// searching the key element in the 2D array in the efficient way with the less time complaxity 
+
+import java.util.*;
+
+public class StaircaseSearch {
+
+    public static boolean search(int matrix[][],int key) {
+  
+        int row = 0;
+        int col = matrix[0].length - 1;
+
+        while (row < matrix.length && col >= 0) {
+            if (key == matrix[row][col]) {
+                System.out.println("The Key value is found at (" + row + "," + col + ")");
+                return true;
+            }
+            
+            else if (key < matrix[row][col]) {
+                col--;
+            }
+            
+            else if (key > matrix[row][col]) {
+                row++;
+            }
+        }
+        System.out.println("the key is not found ");
+        return false;
+    }
+
+    public static void main(String[] args) {
+    
+        int array[][]={{10,20,30,40},
+                       {15,25,35,45,},
+                       {27,29,37,48},
+                       {32,33,39,50}};
+        search(array,33);
+    }
+}
